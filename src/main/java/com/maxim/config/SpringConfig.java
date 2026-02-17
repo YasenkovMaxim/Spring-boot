69d3678bbf072fe9b477aa8def8990bd28410846
jasen <jasenkoff.mack@yandex.ru>
2026-02-17 10:48:55 +0300

    add DataBase

--- src/main/java/com/maxim/config/SpringConfig.java
+++ src/main/java/com/maxim/config/SpringConfig.java
@@ -1,21 +1,44 @@
 package com.maxim.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.core.env.Environment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Configuration
 @ComponentScan("com.maxim")
 public class SpringConfig {
+    private Environment environment;
 
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/views/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+    @Autowired
+    public SpringConfig(Environment environment) {
+        this.environment = environment;
     }
 
+    // add BD
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+                    environment.getProperty("spring.datasource.url"),
+                    environment.getProperty("spring.datasource.username"),
+                    environment.getProperty("spring.datasource.password"));
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
 }
