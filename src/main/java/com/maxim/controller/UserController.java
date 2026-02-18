df03ef6e488c94b28af6b02ca6926e221015fa4e
jasen <jasenkoff.mack@yandex.ru>
2026-02-18 10:02:11 +0300

    переход на REST

--- src/main/java/com/maxim/controller/UserController.java
+++ src/main/java/com/maxim/controller/UserController.java
@@ -1,20 +1,22 @@
 package com.maxim.controller;
 
+import com.maxim.model.Role;
 import com.maxim.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
     @GetMapping("/{id}")
-    public String getUserById(@PathVariable("id") int id, Model model) {
+    public User getUserById(@PathVariable("id") int id) {
         //Temp
         User user = new User();
         user.setAge(22);
@@ -24,8 +26,6 @@ public class UserController {
         user.setLastName("Yasenkau");
         user.setCreated(Instant.now());
         user.setUpdated(Instant.now());
-
-        model.addAttribute("user", user);
-        return "get-user";
+        return user;
     }
 }
