f74c379d34d55e544511f5cfdb7d7337d28e1fe9
jasen <jasenkoff.mack@yandex.ru>
2026-02-21 00:07:52 +0300

    work with DB

--- src/main/java/com/maxim/model/Security.java
+++ src/main/java/com/maxim/model/Security.java
@@ -1,14 +1,26 @@
 package com.maxim.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.Data;
 
 import java.time.Instant;
 
+@Entity(name = "security")
 @Data
 public class Security {
+    @Id
+    @SequenceGenerator(name = "seqSecurityId", sequenceName = "security_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "seqSecurityId")
     private Integer id;
     private String username;
     private String password;
+    @Enumerated(EnumType.STRING)
     private Role role;
     private String userId;
     private Instant created;
--- src/main/java/com/maxim/model/User.java
+++ src/main/java/com/maxim/model/User.java
@@ -1,11 +1,19 @@
 package com.maxim.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.Data;
 
 import java.time.Instant;
 
+@Entity(name = "users")
 @Data
 public class User {
+    @Id
+    @SequenceGenerator(name = "seqUserId", sequenceName = "users_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "seqUserId")
     private Integer id;
     private String firstName;
     private String lastName;
